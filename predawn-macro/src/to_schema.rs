3c0ec49064557d9d789b568adeec601efdc78ce7
ZihanType <ZihanType@protonmail.com>
2024-05-13 09:54:42 +0800

    feat: add a `title` field to the schema generated by the `ToSchema` derived macro

--- predawn-macro/src/to_schema.rs
+++ predawn-macro/src/to_schema.rs
@@ -1,6 +1,7 @@
 use proc_macro2::TokenStream;
+use quote::quote;
 use quote_use::quote_use;
-use syn::{DeriveInput, Field};
+use syn::{DeriveInput, Field, Generics};
 
 use crate::serde_attr::SerdeAttr;
 
@@ -34,12 +35,14 @@ pub(crate) fn generate(input: DeriveInput) -> syn::Result<TokenStream> {
         return Err(e);
     }
 
+    let schema_title = generate_schema_title(&ident.to_string(), &generics);
+
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let expand = quote_use! {
         # use core::default::Default;
         # use predawn::ToSchema;
-        # use predawn::openapi::{Schema, ObjectType, SchemaKind, Type};
+        # use predawn::openapi::{Schema, ObjectType, SchemaData, SchemaKind, Type};
 
         impl #impl_generics ToSchema for #ident #ty_generics #where_clause {
             fn schema() -> Schema {
@@ -47,8 +50,13 @@ pub(crate) fn generate(input: DeriveInput) -> syn::Result<TokenStream> {
 
                 #(#properties)*
 
+                let title = #schema_title;
+
                 Schema {
-                    schema_data: Default::default(),
+                    schema_data: SchemaData {
+                        title: Some(title),
+                        ..Default::default()
+                    },
                     schema_kind: SchemaKind::Type(Type::Object(ty)),
                 }
             }
@@ -89,4 +97,94 @@ fn generate_single_field(field: Field) -> syn::Result<TokenStream> {
     };
 
     Ok(expand)
+}
+
+fn generate_schema_title(name: &str, generics: &Generics) -> TokenStream {
+    let mut have_first = false;
+
+    let types = generics
+        .params
+        .iter()
+        .filter_map(|param| match param {
+            syn::GenericParam::Type(ty) => {
+                let ty = &ty.ident;
+
+                let extract_title = quote_use! {
+                    # use predawn::ToSchema;
+
+                    let schema = <#ty as ToSchema>::schema();
+                    let title = schema.schema_data.title.as_deref().unwrap_or("Unknown");
+                };
+
+                let push_comma = if !have_first {
+                    have_first = true;
+
+                    TokenStream::new()
+                } else {
+                    quote! {
+                        name.push_str(", ");
+                    }
+                };
+
+                let push_title = quote! {
+                    name.push_str(title);
+                };
+
+                Some(quote! {{
+                    #extract_title
+                    #push_comma
+                    #push_title
+                }})
+            }
+            syn::GenericParam::Const(cns) => {
+                let cns = &cns.ident;
+
+                let push_comma = if !have_first {
+                    have_first = true;
+
+                    TokenStream::new()
+                } else {
+                    quote! {
+                        name.push_str(", ");
+                    }
+                };
+
+                let push_title = quote_use! {
+                    # use std::string::ToString;
+
+                    name.push_str(&<#cns as ToString>::to_string());
+                };
+
+                Some(quote! {{
+                    #push_comma
+                    #push_title
+                }})
+            }
+            syn::GenericParam::Lifetime(_) => None,
+        })
+        .collect::<Vec<_>>();
+
+    if types.is_empty() {
+        quote_use! {
+            # use std::string::ToString;
+
+            {
+                ToString::to_string(#name)
+            }
+        }
+    } else {
+        quote_use! {
+            # use std::string::ToString;
+
+            {
+                let mut name = ToString::to_string(#name);
+
+                name.push('<');
+
+                #(#types)*
+
+                name.push('>');
+            }
+        }
+    }
 }
